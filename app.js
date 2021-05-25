cd1cfcfe932324cd8ec6d879f0f1f1127c7df8bc
Manya Pandey <72375909+manyapnd17@users.noreply.github.com>
2021-05-25 20:21:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "Hello, there everyone!! This is the home page of the new blog website created by me . I warmly welcome you here . This is Manya Pandey . I am an innovative individual who comes up with ingenious ideas aand work upon them to increase my real world knowledge. I'm a Full Stack Developer and my work is based mainly upon node.js and React . You can have a look on all the pages of website. You can read our contents and acknowledge it . ";
+const aboutContent = "This blog website is an example of Back-End Development. It built upon Node ,Express and Ejs. You can visit to various blog posts and have a look on them";
+const contactContent = "Email: xyz@gmail.com";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", function(req, res){
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
